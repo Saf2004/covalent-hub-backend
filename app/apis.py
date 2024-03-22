39362abd24e250fdc5c8c12a2ab4e3224cca0d46
safey <s-safey.yahia@zewailcity.edu.eg>
2024-03-22 20:16:06 +0200

    API (feat): added register flask api

--- app/apis.py
+++ app/apis.py
@@ -36,6 +36,39 @@ def user_login():
     else:
         return 'Not found', 404
 
+@app.route('/user-register', methods=['POST'])
+def user_register():
+    uri = "mongodb+srv://covalenthub.iqzz0en.mongodb.net/?authSource=%24external&authMechanism=MONGODB-X509&retryWrites=true&w=majority"
+    client = MongoClient(uri,
+                         tls=True,
+                         tlsCertificateKeyFile='certs/db.pem',
+                         server_api=ServerApi('1'))
+
+    db = client['users']
+    collection = db['credentials']
+    import bcrypt
+
+    password = request.json['password']
+    hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+    if collection.find_one({'email': request.json['email']}):
+        return 'User already exists', 400
+    else:
+        from uuid import uuid4
+        import datetime
+        collection.insert_one({'email': request.json['email'], 'password': hashed_password})
+        user_id = collection.find_one({'email': request.json['email']})['_id']
+        collection = db['info']
+        collection.insert_one({'user_id': user_id, 'name': request.json['name'], 'username': request.json['username'],
+                               'age': request.json['age'], 'country': request.json['country'],
+                               'email': request.json['email'], 'phone': request.json['phone'],
+                               'password': hashed_password})
+        db = client['sessions']
+        collection = db['tokens']
+
+        rand_token = uuid4()
+        collection.insert_one({'user_id': user_id, 'token': rand_token, 'created_at': datetime.datetime.now()})
+
+        return jsonify({'token': rand_token}), 201
 
 if __name__ == '__main__':
     app.run()
